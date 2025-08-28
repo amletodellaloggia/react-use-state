4b665cebf3be1bee6d468ca04c0d4fb7e3444d9b
amletodellaloggia <amletodellaloggia@gmail.com>
2025-08-28 16:31:46 +0200

    [add static representation, imported list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,61 @@
+const languages = [
+  {
+    id: 1,
+    title: "HTML",
+    description:
+      "HTML (HyperText Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini.",
+  },
+  {
+    id: 2,
+    title: "CSS",
+    description:
+      "CSS (Cascading Style Sheets) è un linguaggio di stile utilizzato per descrivere la presentazione di un documento scritto in HTML o XML. Controlla il layout, i colori, i caratteri e l'aspetto complessivo di una pagina web.",
+  },
+  {
+    id: 3,
+    title: "JavaScript",
+    description:
+      "JavaScript è un linguaggio di programmazione dinamico utilizzato per lo sviluppo web, che consente contenuti interattivi come grafici animati, pulsanti cliccabili e altre funzionalità dinamiche sui siti web.",
+  },
+  {
+    id: 4,
+    title: "Node.js",
+    description:
+      "Node.js è un ambiente di runtime JavaScript lato server che permette di utilizzare JavaScript anche sul backend, consentendo agli sviluppatori di usare lo stesso linguaggio sia sul client che sul server.",
+  },
+  {
+    id: 5,
+    title: "Express",
+    description:
+      "Express è un framework per applicazioni web minimalista e veloce per Node.js. Fornisce strumenti robusti per creare applicazioni web e API e semplifica la gestione delle rotte e delle richieste.",
+  },
+  {
+    id: 6,
+    title: "ReactJS",
+    description:
+      "ReactJS è una libreria JavaScript per costruire interfacce utente, in particolare applicazioni a pagina singola. Consente agli sviluppatori di creare componenti UI riutilizzabili e di gestire efficacemente lo stato dell'applicazione.",
+  },
+];
+
 const Main = () => {
+  const defaultLanguage = languages[0];
+
   return (
     <main>
-		<h1>LEARN WEB DEVELOPMENT</h1>
-      <div class="card text-start w-50">
-        <div class="card-header">
-          <ul class="nav nav-pills card-header-pills">
-            <li class="btn btn-primary">
-              <a class="nav-link active" href="#">
-                HTML
-              </a>
-            </li>
+      <h1>LEARN WEB DEVELOPMENT</h1>
+      <div className="card text-start w-50">
+        <div className="card-header">
+          <ul className="nav nav-pills card-header-pills">
+            {languages.map((lang) => (
+              <li>
+                <button  key={lang.id} className="btn btn-primary">{lang.title}</button>
+              </li>
+            ))}
           </ul>
         </div>
-        <div class="card-body">
-          <h5 class="card-title">HTML</h5>
-          <p class="card-text">
-            HTML (HyperText Markup Language) è il linguaggio standard per creare
-            pagine e applicazioni web. Struttura il contenuto web e fornisce
-            elementi di base come titoli, paragrafi e immagini.
-          </p>
+        <div className="card-body">
+          <h5 className="card-title">{defaultLanguage.title}</h5>
+          <p className="card-text">{defaultLanguage.description}</p>
         </div>
       </div>
     </main>
