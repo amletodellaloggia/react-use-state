cb500596e755a5c0fa0de8387880875363eb798b
amletodellaloggia <amletodellaloggia@gmail.com>
2025-08-28 13:59:03 +0200

    [upd] removed test, added static card, css [add] provided array for later use

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,28 @@
 const Main = () => {
+  return (
+    <main>
+		<h1>LEARN WEB DEVELOPMENT</h1>
+      <div class="card text-start w-50">
+        <div class="card-header">
+          <ul class="nav nav-pills card-header-pills">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">
+                HTML
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">HTML</h5>
+          <p class="card-text">
+            HTML (HyperText Markup Language) è il linguaggio standard per creare
+            pagine e applicazioni web. Struttura il contenuto web e fornisce
+            elementi di base come titoli, paragrafi e immagini.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-	return (
-		<main>
-			<h2>Hello World</h2>
-		</main>
-	)
-
-}
-
-export default Main
+export default Main;
